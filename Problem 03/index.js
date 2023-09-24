7055ff44e81cc012e8c05842b0cc95af3db0d3fc
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2023-09-24 14:34:39 +0600

    Problem 03 was solved

---
+++ Problem 03/index.js
@@ -0,0 +1,5 @@
+// Write a JavaScript program to set paragraph background color.
+const func = () =>{
+     let element = document.getElementById('paragraph');
+     element.style.backgroundColor = 'pink';
+}
