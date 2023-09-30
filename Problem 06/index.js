b63c1395c317f1c65c21f873a607b0d3c595b990
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2023-09-30 15:27:37 +0600

    Create a Div Element using DOM and Set className for this element

---
+++ Problem 06/index.js
@@ -0,0 +1,13 @@
+
+//? Create a Div Element using DOM and Set className for this element
+
+const new_div = document.createElement('div');
+//? First Class Name
+new_div.classList.add('new-div');
+document.body.appendChild(new_div);
+//? Second Class Name 
+new_div.className +=' Tarek-div';
+
+
+//? Remove a Existing Class Name from an element using DOM
+
